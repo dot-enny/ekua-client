9f9f59099bd7b0d784896752c608f60fa22fb7cb
dot-enny <olaniyaneniola0225@gmail.com>
2024-12-03 17:09:03 +0100

    test: signup in prod.

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { SignIn } from './pages/auth/SignIn';
 import { SignUp } from './pages/auth/SignUp';
 import { AuthLayout } from './layouts/Auth';
 import { Verify } from './pages/auth/Verify';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import { SignIn } from './pages/SignIn.tsx'
 // import { SignUp } from './pages/SignUp.tsx'
 // import { AuthLayout } from './layouts/Auth.tsx'
@@ -29,8 +30,12 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,27 +1,56 @@
-import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import axios from 'axios';
+
+const BASE_URL = 'http://enny123-001-site1.qtempurl.com/api/';
 
 export const SignUp = () => {
     const navigate = useNavigate();
 
-    const [isLoading, setIsLoading] = useState(false);
-
 	const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-        navigate('/auth/verify');
-		// setIsLoading(true);
-		// const formData = new FormData(e.target as HTMLFormElement);
-		// const { username, email, password } = Object.fromEntries(formData);
+		const formData = new FormData(e.target as HTMLFormElement);
+		const { firstname, lastname, email, password, confirmPassword } = Object.fromEntries(formData) as { [key: string]: string };
+
+		console.log({ firstname, lastname, email, password, confirmPassword });
+		signUpMutation({ firstname, lastname, email, password, confirmPassword });
 	};
+
+	const signUp = async (userData: { firstname: string; lastname: string; email: string; password: string; confirmPassword: string }) => {
+		const response = await axios.post(`${BASE_URL}auth/register`, userData, {
+			headers: {
+				"Content-Type": "application/json",
+				// "Access-Control-Request-Method": "POST",
+				// "Origin": "http://localhost:5173",
+				// "Access-Control-Allow-Origin": "*",
+			},
+			// withCredentials: true, // If credentials like cookies are required
+		}
+		);
+		return response.data;
+	};
+
+	const { mutateAsync: signUpMutation, isPending } = useMutation({
+		mutationFn: signUp,
+		onSuccess: (data: any) => {
+			console.log('User signed up successfully:', data);
+			navigate('/');
+		},
+		onError: (error: any) => {
+			console.error('Error signing up:', error);
+		},
+	});
 
 	return (
 		<div>
 			<h2>Create an Account</h2>
 			<form onSubmit={handleSignUp}>
-				<input type="text" placeholder="username" name="username" required />
+				<input type="text" placeholder="firstname" name="firstname" required />
+				<input type="text" placeholder="lastname" name="lastname" required />
 				<input type="email" placeholder="email" name="email" required />
 				<input type="password" placeholder="password" name="password" required />
-				<button disabled={isLoading}>{isLoading ? 'Loading...' :'Sign Up'}</button>
+				<input type="password" placeholder="confirm password" name="confirmPassword" required />
+				<button disabled={isPending}>{isPending ? 'Loading...' :'Sign Up'}</button>
 				<div className="auth-form__secondary-cta">
 					<span>Already have an account ?</span>
 					<Link to="/auth/sign-in">Login</Link>
